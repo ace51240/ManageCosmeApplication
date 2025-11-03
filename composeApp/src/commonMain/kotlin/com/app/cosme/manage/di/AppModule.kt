ee1af503507082568f69a5ac74ca97db24bdd9a1
AchisCat <aceace040@gmail.com>
2025-11-03 14:56:30 +0900

    feat: integrate Koin for dependency injection
    Adds Koin to the project to manage dependencies. This includes:
    - Adding the Koin library dependencies for `commonMain` and `androidMain`.
    - Creating a `MainApplication` class on Android to initialize Koin.
    - Setting up multiplatform modules (`AppModule.kt`) for Android, iOS, and common code.

---
+++ composeApp/src/commonMain/kotlin/com/app/cosme/manage/di/AppModule.kt
@@ -0,0 +1,5 @@
+package com.app.cosme.manage.di
+
+import org.koin.core.module.Module
+
+expect val platformModule: Module
---
+++ composeApp/src/iosMain/kotlin/com/app/cosme/manage/di/AppModule.kt
@@ -0,0 +1,8 @@
+package com.app.cosme.manage.di
+
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+actual val platformModule: Module = module {
+    // ここにiOS固有の依存関係を定義します
+}
